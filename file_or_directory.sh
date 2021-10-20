eb576eb9e654e68ade7abdfe3f3b3838d8922348
Gayane Mirakyan <gayane_mirakyan01@mail.ru>
2021-10-20 13:00:06 -0700

    Add case when input is empty

--- file_or_directory.sh
+++ file_or_directory.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [ -d $1 ]
-then 
-	echo 'This file is directory'
-elif [ -f $1 ]
+if [ -z $1 ]
 then
-	echo 'This is a regular file'
+	echo 'Empty string'
 else
-	echo 'This file does not exist'
+	if [ -d $1 ]
+	then
+		echo 'This file is directory'
+	elif [ -f $1 ]
+	then
+		echo 'This is a regular file'
+	else
+		echo 'This file does not exist'
+	fi	
 fi
