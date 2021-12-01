ce9104cd444c0fb1593fe3036d75c24772d1a6b4
GayaneMirakyan <gayane_mirakyan01@mail.ru>
2021-12-01 02:02:15 -0800

    Fix bugs

--- Graph/Tests/test2.cpp
+++ Graph/Tests/test2.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 
-int main() 
+int main()
 {
-    int n = 10000, m = 19897, start = 1, finish = 9001;
+    int n = 10000, m = 19800, start = 1, finish = 245;
 
-    std::cout << n << " " << " " << m << " " << start << " " << finish << std::endl;
-    int i;
-    for(i = 1; i < n - 100; i++) {
-        std::cout << i << " " << i + 1 << " " << 1 << std::endl;
+    std::cout << n << " " << m << " " << start << " " << finish << std::endl;
+    int k = 99, s = 0;
+    while(k){
+        int i = s+1;
+        for(i; i < 100+s; i++) 
+	{
+            std::cout << i << " " << i + 1 << " " << 1 << std::endl;
+            std::cout << i << " " << i+100 << " " << 1 << std::endl;
+        }
+        s+=100;
+        k--;
+    }
+    for(int i = 100; i < 10000; i+=100)
+    {
         std::cout << i << " " << i+100 << " " << 1 << std::endl;
     }
-    for(int i = 9901; i < n; i++) {
+
+    for( int i = 9901; i < n; i++) 
+    {
         std::cout << i << " " << i+1 << " " << 1 << std::endl;
     }
-    std::cout << i << " " << i+1 << " " << 1 << std::endl;
 
     return 0;
 }
-
