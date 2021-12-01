430bec739fde88d472e031e59a70e2cb7077f846
GayaneMirakyan <gayane_mirakyan01@mail.ru>
2021-12-01 02:00:27 -0800

    Fix bugs

--- Graph/build.sh
+++ Graph/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 g++ GraphShortestPath.cpp -o main
+cd Tests
 g++ test1.cpp -o test1
 g++ test2.cpp -o test2
+cd ..
--- Graph/clean.sh
+++ Graph/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 rm main
+cd Tests
 rm test1
 rm test2
+cd ..
